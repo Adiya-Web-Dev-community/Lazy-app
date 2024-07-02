15cb700b928bbba42ac933f13b5a12d7e76c0411
Mansoor Khan <mansoorKh1098@gmail.com>
2024-07-02 12:37:23 +0530

    integrated company api edit, delete, creat and creat aleart dialog model

--- admin/src/pages/Companies.tsx
+++ admin/src/pages/Companies.tsx
@@ -11,22 +11,70 @@ import {
   companiesData,
   listHeadingCompanies,
 } from "../components/content_data/contentData";
+import useCompanies from "../hooks/useCompanies";
+import { useDispatch } from "react-redux";
+import { addingData } from "../store/companies";
+import ConfirmDeleteModal from "../components/modal/ConfirmDeleteModal";
+import { useState } from "react";
+import ConfirmationDialog from "../components/modal/ConfirmationDialog";
 
 const Companies = () => {
   const navigate = useNavigate();
+  const dispatch = useDispatch();
 
-  // const { isPending, isError, data, error, refetch } = useQuery<
-  //   ApiResponse<MenuDataResponse>,
-  //   ApiError
-  // >({
-  //   queryKey: ["product"],
-  //   queryFn: async () => {
-  //     return await apiRequest<MenuDataResponse>({
-  //       url: "/menus",
-  //       method: "get",
-  //     });
-  //   },
-  // });
+  const [isDeletModal, setDeletModal] = useState({
+    delet: false,
+    deletElementId: "",
+  });
+  const { isPending, isError, data, error, refetch } = useCompanies();
+
+  const companyData = data?.data?.data;
+  console.log(companyData, "companies");
+
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 5;
+  //calculation of page
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+
+  const currentCompanies = companyData?.slice(
+    indexOfFirstItem,
+    indexOfLastItem
+  );
+
+  console.log(currentCompanies, "pagination");
+
+  const totalPages = Math.ceil(companyData?.length / itemsPerPage);
+
+  const handleClick = (pageNumber: number) => {
+    setCurrentPage(pageNumber);
+  };
+
+  const [dialogCrendial, setDialogCrendial] = useState({
+    targetUrl: "",
+    showDialog: false,
+  });
+
+  const handleLinkClick = (url) => {
+    setDialogCrendial((prev) => ({
+      ...prev,
+      targetUrl: url,
+      showDialog: true,
+    }));
+  };
+
+  const handleCloseDialog = () => {
+    setDialogCrendial((prev) => ({
+      ...prev,
+      targetUrl: "",
+      showDialog: false,
+    }));
+  };
+
+  const handleConfirmRedirect = () => {
+    window.open(dialogCrendial.targetUrl, "_blank");
+    handleCloseDialog();
+  };
 
   const mutation = useMutation<
     ApiResponse<DeletElementData>,
@@ -54,223 +102,255 @@ const Companies = () => {
     },
     onSuccess: (data: ApiResponse<DeletElementData>) => {
       console.log(data);
-      // refetch();
+      refetch();
       toast.dismiss();
       toast.success(`${data?.data?.message}`);
+      setDeletModal((prev) => ({
+        ...prev,
+        delet: false,
+        deletElementId: "",
+      }));
     },
     onError: (error: ApiError) => {
       console.log(error);
+      toast.dismiss();
+      setDeletModal((prev) => ({
+        ...prev,
+        delet: false,
+        deletElementId: "",
+      }));
       toast.error(`${error.message}`);
     },
   });
 
-  const deletMenue = (id) => {
-    const isConfirmed = window.confirm(
-      "Are you sure you want to delete this Dish?"
-    );
+  const deletcompany = (id) => {
+    // const isConfirmed = window.confirm(
+    //   "Are you sure you want to delete this Dish?"
+    // );
 
-    if (isConfirmed) {
-      console.log(id, "delet");
+    // if (isConfirmed) {
+    //   console.log(id, "delet");
 
-      const deleteObj: DishDelet = {
-        path: `/menus/${id}`,
-      };
+    //   const deleteObj: UniDelet = {
+    //     path: `/menus/${id}`,
+    //   };
 
-      console.log(deleteObj);
+    //   console.log(deleteObj);
 
-      // Proceed with the deletion
-      mutation.mutate(deleteObj);
-    } else {
-      // Deletion canceled by the user
-      console.log("Deletion canceled");
-    }
+    //   // Proceed with the deletion
+    //   mutation.mutate(deleteObj);
+    // } else {
+    //   // Deletion canceled by the user
+    //   console.log("Deletion canceled");
+    // }
+
+    setDeletModal((prev) => ({
+      ...prev,
+      delet: true,
+      deletElementId: id,
+    }));
   };
 
-  const updateMenue = (dishData) => {
-    // dispatch(addDishData(dishData));
-    navigate("/product/form");
+  const updatecompany = (data) => {
+    dispatch(addingData(data));
+    navigate("/companies/form");
   };
 
-  // console.log(data, error, "menus");
+  const closehandler = () => {
+    setDeletModal((prev) => ({
+      ...prev,
+      delet: false,
+      deletElementId: "",
+    }));
+  };
 
-  // if (isPending) {
-  //   return <span>Loading...</span>;
-  // }
+  const confirmhandler = () => {
+    const deleteObj: UniDelet = {
+      path: `/api/company/delete/${isDeletModal?.deletElementId}`,
+    };
 
-  // if (isError) {
-  //   return <span>Error: {error.message}</span>;
-  // }
+    console.log(deleteObj);
 
-  // const productData = data?.data?.data;
-
-  // console.log(menueData?.[30].ingredient.split(","));
+    // Proceed with the deletion
+    mutation.mutate(deleteObj);
+  };
 
   return (
-    <section
-      className={`  md:pl-0 p-4 h-full rounded-md font-philosopher  mx-auto [&::-webkit-scrollbar]:hidden`}
-    >
+    <>
+      {isDeletModal?.delet && (
+        <ConfirmDeleteModal onClose={closehandler} onConfirm={confirmhandler} />
+      )}
       <section
-        className={` md:p-8 p-6
+        className={`  md:pl-0 p-4 h-full rounded-md font-philosopher  mx-auto [&::-webkit-scrollbar]:hidden`}
+      >
+        <section
+          className={` md:p-8 p-6
   
 h-full
         border-gray-200 
     rounded-md  font-philosopher max-w-full w-full shadow-md`}
-      >
-        <div className="flex items-center mb-2 md:mb-6">
-          <h1 className=" text-[28px] font-bold md:text-4xl text-[#DEE1E2]">
-            Companies
-          </h1>
-        </div>
-        <div className="flex justify-between mb-4">
-          <div className={`flex items-center   `}>
-            <input
-              type="search"
-              placeholder={`Search`}
-              className={` p-2 text-sm md:text-base  sm:px-4 py-1 border-[2px] border-transparent 
+        >
+          <div className="flex items-center mb-2 md:mb-6">
+            <h1 className=" text-[28px] font-bold md:text-4xl text-[#DEE1E2]">
+              Companies
+            </h1>
+          </div>
+          <div className="flex justify-between mb-4">
+            <div className={`flex items-center   `}>
+              <input
+                type="search"
+                placeholder={`Search`}
+                className={` p-2 text-sm md:text-base  sm:px-4 py-1 border-[2px] border-transparent 
            bg-[#252525] focus:border-gray-800
         shadow-inner rounded-[0.26rem] outline-none `}
-              // value={searchQuery}
-              // onChange={(e) => setSearchQuery(e.target.value)}
-              // onFocus={() => setCurrentPage(1)}
-            />
-          </div>
-          <div className="relative flex items-center self-end ">
-            <button
-              className={` px-2 py-1 
+                // value={searchQuery}
+                // onChange={(e) => setSearchQuery(e.target.value)}
+                // onFocus={() => setCurrentPage(1)}
+              />
+            </div>
+            <div className="relative flex items-center self-end ">
+              <button
+                className={` px-2 py-1 
                    bg-emerald-800  hover:bg-emerald-700 text-[#DEE1E2] font-semibold
               }    rounded shadow-xl md:px-4 md:py-2  sm:self-center`}
-            >
-              <Link to={"/companies/form"}>
-                <span className="hidden md:inline-block">Add Company</span>
+              >
+                <Link to={"/companies/form"}>
+                  <span className="hidden md:inline-block">Add Company</span>
 
-                <IoIosSend className="w-6 h-6 md:hidden" />
-              </Link>
-            </button>
+                  <IoIosSend className="w-6 h-6 md:hidden" />
+                </Link>
+              </button>
+            </div>
           </div>
-        </div>
-        <section
-          className={`w-full overflow-auto   border-2 [&::-webkit-scrollbar]:hidden rounded-lg border-[#1A1A1A] shadow-md bg-[#1A1A1A]`}
-        >
-          <section className="grid grid-cols-customCompanies pb-2 p-2  gap-4  text-[#DEE1E2] min-w-[1200px] font-medium md:font-semibold bg-[#1A1A1A]">
-            <p className="pl-2 md:text-lg">SrNo.</p>
+          <section
+            className={`w-full overflow-auto   border-2 [&::-webkit-scrollbar]:hidden rounded-lg border-[#1A1A1A] shadow-md bg-[#1A1A1A]`}
+          >
+            <section className="grid grid-cols-customCompanies pb-2 p-2  gap-4  text-[#DEE1E2] min-w-[1200px] font-medium md:font-semibold bg-[#1A1A1A]">
+              <p className="pl-2 md:text-lg">SrNo.</p>
 
-            {listHeadingCompanies.map((heading, index) => (
-              <p
-                key={index}
-                className={`   md:text-lg ${
-                  index !== 0 && index !== 1 ? "justify-self-center" : "ml-10"
-                }`}
-              >
-                {heading.charAt(0).toUpperCase() + heading.slice(1)}
-              </p>
-            ))}
-          </section>
-          <div className=" h-[400px] overflow-y-auto [&::-webkit-scrollbar]:hidden min-w-[1200px] bg-[#252525]">
-            {companiesData?.map((company, i) => (
-              <section
-                key={i}
-                className="grid items-center gap-6 py-2 pl-6 pr-4 border-t-2 text-[#DEE1E2] border-[#1A1A1A] grid-cols-customCompanies group hover:bg-[#2c2c2c]"
-              >
-                <span>{i + 1}</span>
-
-                {/* <span
-                  className={` text-xs font-bold  text-center rounded-full   ${
-                    company?.dietry?.toLowerCase() === "veg"
-                      ? "text-green-600 bg-green-100 p-2 text-center"
-                      : company?.dietry?.toLowerCase() === "non-veg"
-                      ? "text-rose-500 bg-rose-100 p-2 text-center"
-                      : ""
+              {listHeadingCompanies.map((heading, index) => (
+                <p
+                  key={index}
+                  className={`   md:text-lg ${
+                    index !== 0 ? "justify-self-center" : "ml-10"
                   }`}
                 >
-                  {company?.dietry ? company?.dietry : "-- --"}
-                </span> */}
-                <span className={`  font-semibold text-center  rounded-full  `}>
-                  {company?.companyName}
-                </span>
-                {/* <div className="flex items-center justify-center">
-                  {company?.image ? (
-                    <img
-                      src={company?.image}
-                      alt="user Image"
-                      className="w-24 h-10 rounded-lg"
-                    />
-                  ) : (
-                    <span className="text-sm font-bold text-gray-400">
-                      No Image
-                    </span>
-                  )}
-                </div> */}
-                {/* <span className="text-sm font-semibold md:text-base">
-                  {company?.contactPerson}
-                </span> */}
-                {/* <span className="w-4 ml-2 text-sm font-semibold rounded-full ">
-              {}
-            </span> */}
-                <span className="text-sm font-semibold break-words break-all text-ellipsis">
-                  {company?.email}
-                </span>
-                <span className="flex justify-center ml-2 text-sm font-semibold ">
-                  {company?.phoneNumber}
-                </span>
-                <span className="flex justify-center ml-2 text-sm font-semibold ">
-                  {company?.address}
-                </span>
-                <span className="ml-2 text-sm font-semibold underline break-words break-all cursor-pointer text-sky-400">
-                  {company?.website}
-                </span>
-                <span className="flex justify-center ml-2 text-sm font-semibold ">
-                  {company?.status}
-                </span>
-                <span className="flex justify-center ml-2 text-sm font-semibold ">
-                  {company?.productsCount}
-                </span>
-                <span className="flex justify-center ml-2 text-sm font-semibold ">
-                  {company?.dateAdded}
-                </span>
-                {/* <div className="flex justify-center text-sm font-semibold md:text-base">
-                  
-                  {company?.ingredient ? (
-                    <span className="flex text-xs font-semibold break-words break-all text-ellipsis md:text-sm">
-                      {company?.ingredient},
-                    </span>
-                  ) : (
-                    <span className="flex justify-center text-sm font-bold text-gray-400">
-                      Empty
-                    </span>
-                  )}
-                </div> */}
-                {/* <span className="flex justify-center ml-2 text-sm font-semibold md:text-base">
-                  ₹ {company?.price}
-                </span> */}
-                {/* <span className="flex justify-center ml-2 text-sm font-semibold md:text-base">
-                  {company?.restaurantId ? company?.restaurantId.name : "---"}
-                </span> */}
-                <div className="grid justify-center gap-2">
-                  <button
-                    className="px-3 py-2 text-sm rounded-md bg-emerald-800 hover:bg-emerald-700"
-                    // onClick={() => updatecompanye(company)}
+                  {heading.charAt(0).toUpperCase() + heading.slice(1)}
+                </p>
+              ))}
+            </section>
+            <div className=" h-[380px] overflow-y-auto [&::-webkit-scrollbar]:hidden min-w-[1200px] bg-[#252525]">
+              {(currentCompanies || companiesData)?.map((company, i) => (
+                <section
+                  key={i}
+                  className="grid items-center gap-6 py-2 pl-6 pr-4 border-t-2 text-[#DEE1E2] border-[#1A1A1A] grid-cols-customCompanies group hover:bg-[#2c2c2c]"
+                >
+                  <span>{i + 1}</span>
+
+                  <span
+                    className={`  font-semibold text-center  rounded-full  `}
                   >
-                    Edit
-                  </button>
-                  <button
-                    className="px-3 py-2 text-sm rounded-md bg-rose-800 hover:bg-rose-700"
-                    // onClick={() => deletcompanye(company._id)}
+                    {company?.name || company?.companyName}
+                  </span>
+
+                  <span className="text-sm font-semibold break-words break-all text-ellipsis">
+                    {company?.email}
+                  </span>
+
+                  <div className="flex items-center justify-center">
+                    {company?.image ? (
+                      <img
+                        src={company?.image}
+                        alt="user Image"
+                        className="w-24 h-10 rounded-lg"
+                      />
+                    ) : (
+                      <span className="text-sm font-bold text-gray-400">
+                        No Image
+                      </span>
+                    )}
+                  </div>
+                  <span className="flex justify-center ml-2 text-sm font-semibold ">
+                    {company?.phoneNumber || "--"}
+                  </span>
+                  <span className="flex justify-center ml-2 text-sm font-semibold ">
+                    {company?.address || "--"}
+                  </span>
+                  <span
+                    onClick={() =>
+                      company?.website && handleLinkClick(company.website)
+                    }
+                    className={`ml-2 text-sm font-semibold text-center ${
+                      company?.website ? "underline text-sky-400 " : ""
+                    } break-words break-all cursor-pointer `}
                   >
-                    Delete
-                  </button>
-                </div>
-              </section>
-            ))}
+                    {company?.website || "----"}
+                  </span>
+                  <ConfirmationDialog
+                    show={dialogCrendial.showDialog}
+                    onClose={handleCloseDialog}
+                    onConfirm={handleConfirmRedirect}
+                  />
+                  <span className="flex justify-center ml-2 text-sm font-semibold ">
+                    {company?.status}
+                  </span>
+                  <span className="flex justify-center ml-2 text-sm font-semibold ">
+                    {company?.productcount || 0}
+                  </span>
+                  {/* <span className="flex justify-center ml-2 text-sm font-semibold ">
+                    {company?.dateAdded || "---"}
+                  </span> */}
+
+                  <div className="grid justify-center gap-2">
+                    <button
+                      className="px-3 py-2 text-sm rounded-md bg-emerald-800 hover:bg-emerald-700"
+                      onClick={() => updatecompany(company)}
+                    >
+                      Edit
+                    </button>
+                    <button
+                      className="px-3 py-2 text-sm rounded-md bg-rose-800 hover:bg-rose-700"
+                      onClick={() => deletcompany(company._id)}
+                    >
+                      Delete
+                    </button>
+                  </div>
+                </section>
+              ))}
+            </div>
+          </section>
+          <div className="flex items-center justify-center w-full mt-4">
+            <div className="flex justify-start w-full">
+              <p className="text-[#DEE1E2] text-sm font-medium">
+                <span>Total Item: </span>{" "}
+                <span>0{currentCompanies?.length}</span>
+              </p>
+            </div>
+            <div className="flex justify-start w-full">
+              {Array.from({ length: totalPages }, (_, index) => (
+                <button
+                  key={index + 1}
+                  className={`mx-1 px-3 py-1  rounded  ${
+                    currentPage === index + 1
+                      ? "bg-emerald-800 text-[#DEE1E2]"
+                      : "bg-gray-400"
+                  }`}
+                  onClick={() => handleClick(index + 1)}
+                >
+                  {index + 1}
+                </button>
+              ))}
+            </div>
           </div>
-        </section>
-        {/* <Pagination
+          {/* <Pagination
       paginationInfo={pagination}
       paginationUpdateds={(value) => setCurrentPage(value)}
       currentPage={currentPage}
       pageNumbers={pageArray}
     /> */}
+        </section>
       </section>
-    </section>
+    </>
   );
 };
 
