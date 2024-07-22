c8c0eb19bb320af168522495a24f04a7a1875f5d
Ajmalshaikh04 <ajshaikh0404@gmail.com>
2024-07-22 15:49:24 +0530

    fixing json parse

--- server/controller/productController.js
+++ server/controller/productController.js
@@ -33,42 +33,16 @@ const GetAllProduct = async (req, res) => {
     });
   }
 };
-// const GetProductById = async (req, res) => {
-//   const { id } = req.params;
-//   try {
-//     const response = await Product.findById(id);
-//     if (!response) {
-//       return res.status(403).json({
-//         success: false,
-//         message: "product not Found",
-//       });
-//     }
-//     res.status(200).json(response);
-//   } catch (error) {
-//     res.status(500).json({
-//       success: false,
-//       message: error.message,
-//     });
-//   }
-// };
-
 const GetProductById = async (req, res) => {
   const { id } = req.params;
   try {
-    let response = await Product.findById(id);
+    const response = await Product.findById(id);
     if (!response) {
-      return res.status(404).json({
+      return res.status(403).json({
         success: false,
-        message: "Product not found",
+        message: "product not Found",
       });
     }
-
-    // Convert the feature array to a string if it's an array
-    if (Array.isArray(response.feature)) {
-      response = response.toObject(); // Convert to a plain JavaScript object
-      response.feature = response.feature.join("");
-    }
-
     res.status(200).json(response);
   } catch (error) {
     res.status(500).json({
