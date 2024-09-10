2ff45661884b0c33fa2430c6c5faf163d8ed6776
Mansoor Khan <mansoorKh1098@gmail.com>
2024-09-10 16:41:39 +0530

    created route for like, save , comment and share

--- server/model/postModel.js
+++ server/model/postModel.js
@@ -1,12 +1,22 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
+const CommentsSchema = new mongoose.Schema({
+  user_id: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  comment: { type: String, required: true },
+  created_at: { type: Date, default: Date.now },
+});
+
 const PostModel = new Schema(
   {
     //   title: { type: String, required: true },
     content: { type: String, required: true },
-    image_url: { type: String },
-    user_id: { type: Schema.Types.ObjectId, ref: "User", required: true },
+    image_url: { type: String }, // Optional image URL
+    user_id: { type: Schema.Types.ObjectId, ref: "User", required: true }, // Reference to the User collection
+    likes: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }], // Stores user IDs who liked the post
+    comments: [CommentsSchema],
+    shares: { type: Number, default: 0 }, // Share count
+    savedBy: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }], // Stores user IDs who saved the post
   },
   {
     timestamps: true,
@@ -15,3 +25,5 @@ const PostModel = new Schema(
 
 const Post = mongoose.model("Post", PostModel);
 module.exports = Post;
+
+// module.exports = mongoose.model("Post", PostSchema);
