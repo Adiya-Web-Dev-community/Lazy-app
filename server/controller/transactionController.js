dfffe661f82bcf1700d3bc9c9b18e792ab5c186a
shahzad <shahzad201415@gmail.com>
2024-09-27 18:51:41 +0530

    changes

--- server/controller/transactionController.js
+++ server/controller/transactionController.js
@@ -1,27 +1,124 @@
 const Transaction=require("../model/transaction");
 
-const createtransaction=async(req,res)=>{
-    try {
-const data = req.body;
-const newTransaction = new Transaction({
-  ...data,
-  userId: req.userId,
-});
-        const savedTransaction = await newTransaction.save();
-        res.status(201).json({
-          success: true,
-          message: "Transaction created successfully",
-          transaction: savedTransaction,
-        });
-      } catch (error) {
-        res.status(500).json({
-          success: false,
-          message: "Failed to create transaction",
-          error: error.message,
-        });
-      }
-}
+const createtransaction = async (req, res) => {
+  try {
+    const data = req.body;
+    const newTransaction = new Transaction({
+      ...data,
+      userId: req.userId,
+    });
+    const savedTransaction = await newTransaction.save();
+    res.status(201).json({
+      success: true,
+      message: "Transaction created successfully",
+      transaction: savedTransaction,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Failed to create transaction",
+      error: error.message,
+    });
+  }
+};
 
+const updateTransaction = async (req, res) => {
+  try {
+    const transactionId = req.params.id;
+    const updatedData = req.body;
+
+    const updatedTransaction = await Transaction.findByIdAndUpdate(
+      transactionId,
+      { $set: updatedData },
+      { new: true }
+    );
+
+    if (!updatedTransaction) {
+      return res.status(404).json({ message: "Transaction not found" });
+    }
+
+    res.status(200).json({
+      success: true,
+      message: "Transaction updated successfully",
+      transaction: updatedTransaction,
+    });
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message });
+  }
+};
+const getAlltransaction = async (req, res) => {
+  try {
+    const transactions = await Transaction.find().populate("userId");
+    res.status(200).json({
+      success: true,
+      data: transactions,
+    });
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message });
+  }
+};
+const getSingletransaction = async (req, res) => {
+  try {
+    const {id}=req.params
+    const transactions = await Transaction.findById(id).populate("userId");
+    res.status(200).json({
+      success: true,
+      data: transactions,
+    });
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message });
+  }
+};
+const getSingletransactionadmin = async (req, res) => {
+  try {
+    const {id}=req.params
+    const transactions = await Transaction.findById(id).populate("userId");
+    res.status(200).json({
+      success: true,
+      data: transactions,
+    });
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message });
+  }
+};
+const getTransactionByuserId = async (req, res) => {
+  try {
+    const userId = req.userId;
+    const transactions = await Transaction.find({ userId: userId }).populate(
+      "userId"
+    );
+
+    if (transactions.length === 0) {
+      return res
+        .status(404)
+        .json({ message: "No transactions found for this user" });
+    }
+
+    res.status(200).json({
+      success: true,
+      data: transactions,
+    });
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message });
+  }
+};
+const getTransactionByuserIdforAdmin = async (req, res) => {
+  try {
+    const userId = req.params.userId;
+    const transactions = await Transaction.find({ userId: userId }).populate(
+      "userId"
+    );
+    res.status(200).json(transactions);
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message });
+  }
+};
 module.exports ={
-    createtransaction
+    createtransaction,
+    updateTransaction,
+    getAlltransaction,
+    getTransactionByuserId,
+    getTransactionByuserIdforAdmin,
+    getSingletransaction,
+    getSingletransactionadmin
 }
--- server/routes/transactionRoute.js
+++ server/routes/transactionRoute.js
@@ -2,9 +2,22 @@ const express = require("express");
 const router = express.Router();
 const { isAdmin,isUser } = require("../middleware/uservalidate");
 const {
-    createtransaction
+    createtransaction,
+    updateTransaction,
+    getAlltransaction,
+    getTransactionByuserId,
+    getTransactionByuserIdforAdmin,
+    getSingletransaction,
+    getSingletransactionadmin
 } = require("../controller/transactionController");
 
 router.post("/",isUser,createtransaction);
+router.put("/:id",isAdmin,updateTransaction);
+router.get("/",isAdmin,getAlltransaction);
+router.get("/:id",isAdmin)
+router.get("/user/all",isUser,getTransactionByuserId)
+router.get("/user/:userId",getTransactionByuserIdforAdmin);
+router.get("/single/:id",isUser,getSingletransaction)
+router.get("/single/admin/:id",isAdmin,getSingletransactionadmin)
 
 module.exports = router;
